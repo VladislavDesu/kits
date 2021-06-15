7cbca049b64021a2482e9d96e56b60c9873f2282
Vladislav Vnukov <vgryadko@gmail.com>
2021-06-15 21:00:22 +0300

    fix

--- gulp/build.js
+++ gulp/build.js
@@ -1,13 +1,13 @@
 const gulp = require("gulp");
 
-const styles = require("./styles"),
-   fonts = require("./fonts"),
-   images = require("./images"),
-   server = require("./server"),
-   clean = require("./clean"),
-   scripts = require("./scripts"),
-   layout = require("./layout");
+const gulpPath = [
+   require("./clean"),
+   require("./fonts"),
+   require("./images"),
+   require("./styles"),
+   require("./scripts"),
+   require("./layout"),
+   require("./server"),
+];
 
-const starter = gulp.series(fonts, images, styles, scripts, layout);
-
-module.exports = gulp.series(clean, starter, server);
+module.exports = gulp.series(...gulpPath);
--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-const ten = 10;
-const five = 5;
-
-if (ten > five) {
-   console.log("yep");
-} else {
-   console.log("no");
-}
