c976a2b3ca101b79744f3f59f2676c0250d6f29a
Vladislav Gryadko <vgryadko.dev@gmail.com>
2021-06-22 13:16:40 +0300

    fix

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,5 +1,5 @@
 module.exports = gulp.task("scripts", () => {
-  return (
+  return mergeStream(
     gulp
       .src(path.dev.scripts)
       // .pipe(webpack(webpackConfig))
@@ -9,6 +9,10 @@ module.exports = gulp.task("scripts", () => {
           suffix: ".min",
         })
       )
+      .pipe(gulp.dest(path.build.scripts)),
+    gulp
+      .src("vendor/bootstrap/js/bootstrap.min.js")
+      // .pipe(webpack(webpackConfig))
       .pipe(gulp.dest(path.build.scripts))
   );
 });
