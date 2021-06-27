1f2cfaca6d5f3532694818e05234b83b259653c7
Vladislav Vnukov <vgryadko@gmail.com>
2021-06-27 19:46:57 +0300

    fix

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -7,6 +7,10 @@ module.exports = gulp.task("vendor", () => {
   const bootstrapSCSS = gulp
     .src("./node_modules/bootstrap/scss/**/*")
     .pipe(gulp.dest("./vendor/bootstrap/scss"));
+  // Jquery
+  const bootstrapSCSS = gulp
+    .src("./node_modules/jquery/dist/")
+    .pipe(gulp.dest("./vendor/jquery/"));
 
   return mergeStream(bootstrapJS, bootstrapSCSS);
 });
